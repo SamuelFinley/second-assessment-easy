1aa2d1bfaa331a511b2a121b9c4d26322db183cb
unknown <sfp3@gatech.edu>
2017-07-28 09:11:47 -0500

    please work

--- Chirp/Chirp/src/main/java/com/chirp/Chirp/repository/ChirpRepository.java
+++ Chirp/Chirp/src/main/java/com/chirp/Chirp/repository/ChirpRepository.java
@@ -3,9 +3,10 @@ package com.chirp.Chirp.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.chirp.Chirp.entity.Chirp;
-
+@Repository
 public interface ChirpRepository extends JpaRepository<Chirp, Integer>{
 	public List<Chirp> findByVisibleTrue();
 //	public Chirp getByVisibleTrueAndUsernameAndPassword(String username, String password);
